c57276e84625c66fcba5d30d1965be0634f670b4
MariaTheresaAzzi <mariatheresaelkazzi@gmail.com>
2024-02-22 16:08:05 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods:{
+    addToCount(){
+      this.count = this.count + 1;
+    },
+    substractToCount(){
+      this.count = this.count - 1;
+    }
+  }
+};
+</script>
+
 <template>
-  <div></div>
+  <div class="all">
+    <h4>The current count ...</h4>
+    <h1>
+      {{ count }}
+    </h1>
+    <div>
+      <button @click="substractToCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.all {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
